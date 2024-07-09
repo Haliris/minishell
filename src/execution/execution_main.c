c088aeaa85f42c99cd7ab8e1cb4efc5ad47f1fb7
Jean Teissier <jteissie@made-f0Br7s2.clusters.42paris.fr>
2024-07-09 17:53:03 +0200

    Lex_parser integration in process, need parser linked list to point both ways

--- src/execution/execution_main.c
+++ src/execution/execution_main.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*   execution_main.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 14:19:59 by jteissie          #+#    #+#             */
-/*   Updated: 2024/06/17 10:51:39 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/07/09 17:52:17 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
-#include "execution.h"
+
+#include "minishell.h"
 
 void	redirect(int *p_fd, int *file_fd)
 {
@@ -21,11 +21,30 @@ void	redirect(int *p_fd, int *file_fd)
 	close(p_fd[1]);
 }
 
-void	process_command(char *cmd, char **env, int *fd)
+int	check_redir(t_lex_parser *parsed)
+{
+	int	redir_count;
+
+	redir_count = 0;
+	if (parsed->prev->type == TK_REDIR)
+		redir_count++;
+	if (parsed->next->type == TK_REDIR)
+		redir_count++;
+	return (redir_count);
+}
+
+void	process_command(t_lex_parser *parsed, char **env, int *child_count)
 {
 	int	pid_child;
+	int	redirection;
 	int	p_fd[2];
 
+	redirection = check_redir(parsed);
+	if (parsed->type == TK_CMD)
+	{
+		execute_single_cmd(parsed, **env);
+		*child_count = *child_count + 1;
+	}
 	if (pipe(p_fd) == -1)
 		handle_error("Could not open pipe for middle child", EXIT_FAILURE);
 	pid_child = fork();
@@ -34,7 +53,7 @@ void	process_command(char *cmd, char **env, int *fd)
 	if (pid_child == 0)
 	{
 		redirect(p_fd, fd);
-		execute(cmd, env);
+		execute(parsed, env);
 	}
 	else
 	{
@@ -70,19 +89,47 @@ void	wait_for_children(int index)
 
 //NEED SUPPORT FOR INFILES AND OUTFILES
 
-int	execute_commands(int cmd_count, char *cmds[] char *envp[], int fds[])
+int	execute_commands(int cmd_count, t_lex_parser *tables, char *envp[])
 {
-	int		index;
+	int				child_count;
+	t_lex_parser	*roaming;
 
-	//file descriptors have to be parsed and properly sorted before being passed to this program
-	index = 0;
-	//legacy command for first command
-	// process_command(av[index++], envp, fd_file, 0);
-	while (index < cmd_count)
-		process_command(cmds[index++], envp, fds[ADD_INDEX], 0);
-	//legacy code for last command
-	//process_command(av[ac - 2], envp, fd_file, LAST_FILE);
-	wait_for_children(index);
-	// Might need to close fds here too since they are inherited
-	exit(EXIT_SUCCESS);
+	child_count = 0;
+	roaming = tables;
+	while (roaming)
+	{
+		if (roaming->type == TK_PIPE || roaming->type == TK_CMD)
+			process_command(roaming, envp, &child_count);
+		roaming = roaming->next;
+	}
+	wait_for_children(child_count);
+	return (1);
+}
+
+int	count_commands(t_lex_parser *tables)
+{
+	int				cmd_count;
+	t_lex_parser	*roaming;
+
+	roaming = tables;
+	cmd_count = 0;
+	while (roaming)
+	{
+		if (roaming->type == TK_PIPE)
+			cmd_count += 2;
+		else if (roaming->TK_cmd_count)
+			cmd_count++;
+		roaming = roaming->next;
+	}
+	return (cmd_count);
+}
+void	process_tables(t_lex_parser *tables, char *envp[])
+{
+	int		cmd_count;
+	int		pid_status;
+
+	cmd_count = count_commands(tables);
+	if (!cmd_count)
+		return ;
+	execute_commands(cmd_count, tables, envp);
 }
