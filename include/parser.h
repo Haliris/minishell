87639208156f4547033c782c4256115c360636a3
Jean Teissier <jteissie@made-f0Br5s5.clusters.42paris.fr>
2024-07-11 12:11:38 +0200

    removed unused function from header

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 13:17:51 by jteissie          #+#    #+#             */
-/*   Updated: 2024/07/11 12:10:48 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/07/11 12:11:30 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,6 @@ typedef struct s_lex_parser
 
 t_lex_parser	*interprete_lexer(t_token *tokens_list);
 int				build_redirect_table(t_lex_parser *parsed, t_token *lexer);
-char			*get_pipe_cmd(t_token *tokens, int mode, t_lex_parser *parsed);
 
 char			*re_join_lexstr(char *lexstr, char *s2, int mode);
 int				check_remaining_tokens(t_token *tokens);
