f7f26827b562ae888d782d960ecc0ed27ed7a971
jteissie <jteissie@42paris.fr>
2024-07-16 21:34:56 +0200

    Modified is_not_number function and replaced ft-printf with ft_putstr_fd to properly print error messages. Still not working properly. std_fds remain opened if the parent calls exit built_in.

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 19:27:00 by jteissie          #+#    #+#             */
-/*   Updated: 2024/07/16 19:38:18 by jteissie         ###   ########.fr       */
+/*   Updated: 2024/07/16 21:31:48 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,23 @@ int	is_not_number(char *str)
 		 index++;
 	while (str[index])
 	{
-		if (str[index] < 0 || str[index] > 9)
-			return (FALSE);
+		if (str[index] > 0 || str[index] < 9)
+			return (TRUE);
 		index++;
 	}
-	return (SUCCESS);
+	return (FALSE);
 }
 
 void	call_exit(char **cmd)
 {
 	int	exit_code;
 
+	if (!cmd || !cmd[1])
+	{
+		trash(cmd);
+		exit(EXIT_SUCCESS);
+	}
+	exit_code = EXIT_FAILURE;
 	if (cmd[2] != NULL)
 	{
 		trash(cmd);
@@ -39,11 +45,13 @@ void	call_exit(char **cmd)
 	}
 	if (is_not_number(cmd[1]) == TRUE)
 	{
-		trash(cmd);
-		ft_printf("exit: %s: numeric argument required\n", cmd[1]);
-		return ;
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
+		ft_putstr_fd("exit: ", STDERR_FILENO);
+		ft_putstr_fd(cmd[1], STDERR_FILENO);
+		ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
 	}
-	exit_code = ft_atoi(cmd[1]);
+	else
+		exit_code = ft_atoi(cmd[1]);
 	trash(cmd);
 	exit(exit_code);
 }
