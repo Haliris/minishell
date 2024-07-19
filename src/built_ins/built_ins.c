e92cc7f24597d57366dc4a1895c61838656337ad
jteissie <jteissie@42paris.fr>
2024-07-19 18:24:28 +0200

    Fixed built_ins not handling redirections by adding process_files() call in execute_builtin()

--- src/built_ins/built_ins.c
+++ src/built_ins/built_ins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   built_ins.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 17:59:53 by jteissie          #+#    #+#             */
-/*   Updated: 2024/07/18 16:58:47 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/07/19 18:22:50 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ void	execute_builtin(char *cmd, t_data *data, int mode)
 	char	**command;
 
 	command = ft_split(cmd, ' ');
+	process_files(data->parsedata);
 	free_parsed_mem(&data->parsedata);
 	free(data->parsedata);
 	if (!command || !command[0])
