2c3e927c4a6b639dfd6173781e0ffc4af2e1f1be
Jean Teissier <jteissie@made-f0Br7s9.clusters.42paris.fr>
2024-07-20 11:57:59 +0200

    Refactoring in progress, creating global signal variable

--- src/signals.c
+++ src/signals.c
@@ -3,18 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bthomas <bthomas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 12:57:23 by bthomas           #+#    #+#             */
-/*   Updated: 2024/07/16 11:06:08 by bthomas          ###   ########.fr       */
+/*   Updated: 2024/07/20 11:33:08 by jteissie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+
 void	sigint(int sig)
 {
 	(void)sig;
+	// interrupt_heredoc(sig);
 	ft_printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 0);
