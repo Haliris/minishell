0d92bfe03cefcfcc3295b3faf4a2b8e986b26c7b
Jean Teissier <jteissie@bess-f2r3s5.clusters.42paris.fr>
2024-08-05 11:45:28 +0200

    switched bash for bash --posix to run tests, added rm -rf .test_commands.txt.swp to avoid annoying vim jank

--- shell_goodman.sh
+++ shell_goodman.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' 
+NC='\033[0m'
 
 MINISHELL_PATH="./minishell"
 TEST_COMMANDS="test_commands.txt"
@@ -25,7 +25,7 @@ if [ ! -f "$TEST_COMMANDS" ]; then
 # Lines starting with # are comments and will be ignored. Empty lines will be ignored.
 
 # The file is named test_commands.txt, the script checks for its name so do not rename it.
-# Adding 'remove' as the last line will make the script remove it after it is done running. 
+# Adding 'remove' as the last line will make the script remove it after it is done running.
 # Otherwise, the file will remain in the working directory and can be editted at will between runs.
 
 # The script will NOT remove any files your minishell has created unless specified in the cleanup() function. By default, it will try to rm output1.txt up to output4.txt.
@@ -48,7 +48,7 @@ cleanup() {
 	kill $MINISHELL_PID
 	# Add any additional files created by the commands here
 	rm -rf output.txt output2.txt output3.txt output4.txt
-
+    rm -rf .test_commands.txt.swp
 	rm -rf "$TEMP_DIR"
 	exit
 }
@@ -66,17 +66,17 @@ trap interrupted SIGINT
 echo -e " ${YELLOW}
 
 
-		  _____ _   _        _____ _          _ _    _____                 _                       
-		 |_   _| | ( )      / ____| |        | | |  / ____|               | |                      
-		   | | | |_|/ ___  | (___ | |__   ___| | | | |  __  ___   ___   __| |_ __ ___   __ _ _ __  
-		   | | | __| / __|  \___ \| '_ \ / _ \ | | | | |_ |/ _ \ / _ \ / _| | |_ | _ \ / _| | \_ \ 
+		  _____ _   _        _____ _          _ _    _____                 _
+		 |_   _| | ( )      / ____| |        | | |  / ____|               | |
+		   | | | |_|/ ___  | (___ | |__   ___| | | | |  __  ___   ___   __| |_ __ ___   __ _ _ __
+		   | | | __| / __|  \___ \| '_ \ / _ \ | | | | |_ |/ _ \ / _ \ / _| | |_ | _ \ / _| | \_ \
 		  _| |_| |_  \__ \  ____) | | | |  __/ | | | |__| | (_) | (_) | (_| | | | | | | (_| | | | |
-		 |_____|\__| |___/ |_____/|_| |_|\___|_|_|  \_____|\___/ \___/ \__,_|_| |_| |_|\__,_|_| |_| 
+		 |_____|\__| |___/ |_____/|_| |_|\___|_|_|  \_____|\___/ \___/ \__,_|_| |_| |_|\__,_|_| |_|
 
- 
+
  ${NC}"
 
-echo -e                        "${YELLOW}                                                                                                                              
+echo -e                        "${YELLOW}
 ________________________________________________________________________________________________________________________________________________________
 
 										 ....... ...............................    ..........................',;,,,'.................'''''',,,,,,;;;;;;::::ccccclllloo
@@ -153,7 +153,7 @@ ________________________________________________________________________________
                                       ...''',,,,',,,,,;;;;,,,,,,,,,''''''..................................,;'......;cc:,'...........',;;;;;,'........
                                        ..''',,,;,,,,;;;;;;;,,,,,,,,''''''...................................... .....,::,'................';:c::;,....
                                        ..''',,;;;;;;;;;;;;;;;;;,,,,'''''''....................................  ......,;;'...................',;clll:;
-									   
+
 ______________________________________________________________________________________________________________________________________________________
 									   ${NC}"
 
@@ -174,7 +174,7 @@ do
     fi
    echo -e "${YELLOW}Testing command: $cmd${NC}"
 
-    bash -c "$cmd" > "$TEMP_DIR/bash_output" 2> "$TEMP_DIR/bash_error"
+    bash --posix -c "$cmd" > "$TEMP_DIR/bash_output" 2> "$TEMP_DIR/bash_error"
 
     truncate -s 0 "$TEMP_DIR/minishell_output"
 
@@ -185,14 +185,14 @@ do
     sed '1d;/minishell>/d' "$TEMP_DIR/minishell_output" > "$TEMP_DIR/minishell_processed"
 
     if ! diff -q "$TEMP_DIR/bash_output" "$TEMP_DIR/minishell_processed" >/dev/null
-    then 
+    then
         echo -e "${RED}Diff KO :(${NC}"
         echo "Diff:"
         diff --color=always "$TEMP_DIR/bash_output" "$TEMP_DIR/minishell_processed"
-        echo 
+        echo
 	else
 		echo -e "${GREEN}Diff ok! :)${NC}"
-    fi  
+    fi
 
 done < "$TEST_COMMANDS"
 
